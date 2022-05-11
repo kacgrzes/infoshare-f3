caaf5c29c914c0886f7ea035ebfc2e52720084f3
Kacper Grzeszczyk <kacgrzes@gmail.com>
2022-05-11 13:32:48 +0200

    Create UserCard

--- libs/shared/ui/src/UserCard/UserCard.native.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { UserCardProps } from "./UserCard.props";
-import { Pressable } from 'react-native'
-
-export const UserCard = (props: UserCardProps) => {
-  return <Pressable style={{}}>
-    
-  </Pressable>
-}
--- libs/shared/ui/src/UserCard/UserCard.tsx
+++ libs/shared/ui/src/UserCard/UserCard.tsx
@@ -1,6 +1,24 @@
-import React from "react"
-import { UserCardProps } from "./UserCard.props";
+import React from 'react';
+import { UserCardProps } from './UserCard.props';
+import { View, Text } from 'react-native';
+import { Button } from '../Button';
+import { Avatar } from '../Avatar';
+import tw from "twrnc";
 
 export const UserCard = (props: UserCardProps) => {
-  return null
-}
+  const { user, onPress } = props
+
+  return (
+    <View style={tw`justify-center items-center`}>
+      <View style={tw`mb-3`}>
+        <Avatar size='big' profileImageUrl={user.profileImageUrl}/>
+      </View>
+      <View style={tw`mb-3`}>
+        <Text>{props.user.name} (@{user.username})</Text>
+      </View>
+      <View style={tw`w-full`}>
+        <Button title={'Delete'} onPress={onPress} />
+      </View>
+    </View>
+  );
+};
